b2292c8f7401add692dc1c385eb41b2f2c42679f
ExpertCoder101 <126113099+ExpertCoder101@users.noreply.github.com>
2023-04-10 16:49:21 -0500

    Update AI-Assistant 1.py

--- AI-Assistant 1.py
+++ AI-Assistant 1.py
@@ -4,6 +4,8 @@ import pyttsx3 as tts
 import sys
 import webbrowser
 import datetime
+import tkinter as tk
+import time
 
 recognizer = speech_recognition.Recognizer()
 
@@ -146,12 +148,12 @@ def youtube_music_tv():
     global recognizer
 
     speaker.say("What do you want to watch or listen to?")
-    speaker.runAndWait()
 
     done = False
 
     while not done:
         try:
+
             with speech_recognition.Microphone() as mic:
 
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
@@ -173,7 +175,7 @@ def youtube_music_tv():
             speaker.runAndWait()
 
 
-def time():
+def clock():
     global recognizer
 
     speaker.say("The time is: ")
@@ -182,6 +184,7 @@ def time():
 
     while not done:
         try:
+
             with speech_recognition.Microphone() as mic:
 
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
@@ -212,6 +215,7 @@ def date():
 
     while not done:
         try:
+
             with speech_recognition.Microphone() as mic:
 
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
@@ -225,6 +229,87 @@ def date():
                 done = True
 
                 speaker.say(formatted_date)
+                speaker.runAndWait()
+
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+            speaker.say("I did not understand. Please try again!")
+            speaker.runAndWait()
+
+
+def stopwatch():
+    global recognizer
+
+    speaker.say("Opening stopwatch app")
+
+    done = False
+
+    while not done:
+        try:
+
+            with speech_recognition.Microphone() as mic:
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                stopwatch = recognizer.recognize_google(audio)
+                stopwatch.lower()
+
+                speaker.say("The App has been opened, all you have to do is press the start button.")
+                speaker.runAndWait()
+
+                # Start of Stopwatch App code
+
+                class StopWatch:
+                    def __init__(self, master):
+                        self.master = master
+                        self.master.title("Stopwatch")
+
+                        self.running = False
+                        self.elapsed_time = 0
+                        self.last_time = 0
+
+                        self.time_label = tk.Label(self.master, text="00:00:00", font=("Arial", 30))
+                        self.time_label.pack(pady=10)
+
+                        self.start_button = tk.Button(self.master, text="Start", command=self.start_stopwatch)
+                        self.start_button.pack(pady=5)
+
+                        self.reset_button = tk.Button(self.master, text="Reset", command=self.reset_stopwatch)
+                        self.reset_button.pack(pady=5)
+
+                    def start_stopwatch(self):
+                        self.running = not self.running
+                        if self.running:
+                            self.start_button.config(text="Stop")
+                            self.update_time()
+                        else:
+                            self.start_button.config(text="Start")
+                            self.master.after_cancel(self.job)
+
+                    def update_time(self):
+                        if self.running:
+                            self.elapsed_time += time.time() - self.last_time
+                            minutes, seconds = divmod(int(self.elapsed_time), 60)
+                            hours, minutes = divmod(minutes, 60)
+                            self.time_label.config(text="{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds))
+                            self.last_time = time.time()
+                            self.job = self.master.after(10, self.update_time)
+
+                    def reset_stopwatch(self):
+                        self.running = False
+                        self.elapsed_time = 0
+                        self.start_button.config(text="Start")
+                        self.time_label.config(text="00:00:00")
+
+                root = tk.Tk()
+                stopwatch = StopWatch(root)
+                root.mainloop()
+
+                # End of Stopwatch App code
+
+                done = True
+
                 speaker.runAndWait()
 
         except speech_recognition.UnknownValueError:
@@ -247,8 +332,9 @@ mappings = {
     "google_search": google_search,
     "youtube_music_tv": youtube_music_tv,
     "exit": quit,
-    "time": time,
-    "date": date
+    "clock": clock,
+    "date": date,
+    "stopwatch": stopwatch
 }
 
 assistant = GenericAssistant('intents.json', intent_methods=mappings)
@@ -268,3 +354,4 @@ while True:
         assistant.request(message)
     except speech_recognition.UnknownValueError:
         recognizer = speech_recognition.Recognizer()
+        
